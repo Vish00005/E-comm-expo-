6a68d4c786c1f527f409ac4f5a214fbd875da87b
Vishal_Khimsuriya__ <vishal.j.khim@gmail.com>
2026-03-10 00:04:47 +0530

    fix routing

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { ClerkProvider } from "@clerk/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home.jsx";
 import Admin from "./Admin.jsx";
@@ -9,15 +8,13 @@ import User from "./User.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/user" element={<User />} />
-        </Routes>
-      </BrowserRouter>
-    </ClerkProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
