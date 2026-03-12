d17a6da64b1cd6761d284305dfb4502ed5e06e8f
Vishal_Khimsuriya__ <vishal.j.khim@gmail.com>
2026-03-12 23:13:00 +0530

    fixing middlewares

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const { Users } = require("../models/user");
 
 module.exports.isAuth = async (req, res, next) => {
   try {
@@ -6,7 +6,7 @@ module.exports.isAuth = async (req, res, next) => {
       return res.status(401).json({ message: "Unauthorized" });
     }
 
-    const user = await User.findById(req.session.user.id);
+    const user = await Users.findById(req.session.user.id);
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
