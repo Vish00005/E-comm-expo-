e16def5b3daeafef9a11fc33d49ccba1fa9d2dfd
Vishal_Khimsuriya__ <vishal.j.khim@gmail.com>
2026-03-12 23:07:09 +0530

    Routes 1.1

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../models/user"); // import model
+const { Users } = require("../models/user"); // import model
 
 router.post("/google", async (req, res) => {
   try {
